4bad0ef051a6c334ac384ad8e1c54cd51d57b371
MUVoDoiKPK <79513408+Elvin-DEV1@users.noreply.github.com>
2021-12-11 20:31:11 +0700

    add post and add comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  root 'static_pages#home'
-  get 'static_pages/home'
-  devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts do
+  	resources :comments
+  end
+
+  root 'posts#index'
 end
