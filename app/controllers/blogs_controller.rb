a3144fb51103052d7207c3a26cf240d51f7b7f87
Sherly1001 <Sherly1001@users.noreply.github.com>
2021-12-13 14:23:55 +0000

    fix: blog controller

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -24,6 +24,7 @@ class BlogsController < ApplicationController
   # POST /blogs or /blogs.json
   def create
     @blog = Blog.new(blog_params)
+    @blog.user = current_user
 
     respond_to do |format|
       if @blog.save
